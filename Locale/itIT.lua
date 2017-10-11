95178b816971c0151ef3e48ef8da13ec86540d48
SafeteeWoW <30888549+SafeteeWoW@users.noreply.github.com>
2017-10-11 16:35:22 -0700

    Add curse autopacking locationization string to other languages

--- Locale/itIT.lua
+++ Locale/itIT.lua
@@ -3,3 +3,11 @@
 
 local L = LibStub("AceLocale-3.0"):NewLocale("RCEPGP", "itIT")
 if not L then return end
+
+-- Manually export from Curse here if you want to update the addon from Github, but not guarantee to be update-to-date.
+--@debug@
+
+--@end-debug@
+
+
+--@localization(locale="itIT", format="lua_additive_table", same-key-is-true=true)@
