4d72bab3866a0bebcf28f320dbe2a2d5dcd9240d
SafeteeWoW <30888549+SafeteeWoW@users.noreply.github.com>
2017-10-11 15:08:20 -0700

    Update and Import Locale

--- Locale/ruRU.lua
+++ Locale/ruRU.lua
@@ -4,47 +4,94 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("RCEPGP", "ruRU")
 if not L then return end
 
+--Translation missing
+-- L["announce_awards_desc2"] = ""
+--Translation missing
+-- L["announce_formula_runtime_error"] = ""
+--Translation missing
+-- L["Award GP (Default: %s)"] = ""
+L["Bidding"] = "ставки"
+--Translation missing
+-- L["bidding_desc"] = ""
+--Translation missing
+-- L["chat_commands"] = ""
+L["Credit GP to %s"] = "Начислить GP для игрока %s"
+L["Custom GP"] = "пользовательское значение GP"
+--Translation missing
+-- L["customGP_desc"] = ""
+L["disable_gp_popup"] = "Всплывающее окно GP автоматически отключается с помощью RCLootCouncil - EPGP."
+L["Enable Bidding"] = "разрешать ставки"
 L["enable_custom_gp"] = "Включить пользовательское значение GP"
---L["formula_help"] = "Введите LUA код, который возвращает значение GP в поле ниже. Вы можете использовать следующие переменные."
-L["formula_syntax_error"] = "Формула содержит синтаксическую ошибку. Вместо нее будет использована формула по умолчанию."
+--Translation missing
+-- L["EPGP_DKP_Reloaded_settings_received"] = ""
+--Translation missing
+-- L["error_no_target"] = ""
+--Translation missing
+-- L["General"] = ""
+L["GP Bid"] = "ставку GP"
 L["gp_formula"] = "Формула GP"
+--Translation missing
+-- L["gp_formula_help"] = ""
+L["gp_formula_syntax_error"] = "Формула содержит синтаксическую ошибку. Вместо нее будет использована формула по умолчанию."
 L["gp_value_help"] = [=[Пример:
 100%: использовать 100% от обычного значения GP
 50%: использовать 50% от обычного значения GP
 25: все вещи стоят 25 GP]=]
+L["gp_variable_equipLoc_help"] = "Строка. Не локализованная строка с названием слота экипировки. Рекомендуется использовать переменную \"slotWeights\" вместо нее"
+L["gp_variable_hasAvoid_help"] = "Целочисленное значение. 1 - если на предмете есть избегание, 0 в противном случае."
+L["gp_variable_hasIndes_help"] = "Целочисленное значение. 1 - если предмет не теряет прочности, 0 в противном случае."
+L["gp_variable_hasLeech_help"] = "Целочисленное значение. 1 - если на предмете есть самоисцеление, 0 в противном случае."
+L["gp_variable_hasSpeed_help"] = "Целочисленное значение. 1 - если предмет c дополнительной скоростью передвижения, 0 в противном случае."
+L["gp_variable_ilvl_help"] = "Целочисленное значение. Уровень предмета или базовый ilvl токена."
+--Translation missing
+-- L["gp_variable_isHeroic_help"] = ""
+--Translation missing
+-- L["gp_variable_isMythic_help"] = ""
+--Translation missing
+-- L["gp_variable_isNormal_help"] = ""
+--Translation missing
+-- L["gp_variable_isTitanforged_help"] = ""
+L["gp_variable_isToken_help"] = "Целочисленное значение. 1 - если это тировый токен, 0 в противном случае."
+--Translation missing
+-- L["gp_variable_isWarforged_help"] = ""
+L["gp_variable_itemID_help"] = "Целочисленное значение. Id предмета."
+--Translation missing
+-- L["gp_variable_link_help"] = ""
+L["gp_variable_numSocket_help"] = "Целочисленное значение. Количество гнезд в предмете."
+L["gp_variable_rarity_help"] = "Целочисленное значение. Редкость предмета. 3-редкий, 4-эпический, 5-легендарный"
+L["gp_variable_slotWeights_help"] = "Число. Вес предмета в зависимости от слота экипировки."
+--Translation missing
+-- L["gpOptions"] = ""
+--Translation missing
+-- L["gpOptionsButton"] = ""
 L["Input must be a number."] = "Значение должно быть числом."
-L["restore_default"] = "Вернуть по умолчанию"
+--Translation missing
+-- L["need_restart_notification"] = ""
+--Translation missing
+-- L["new_version_detected"] = ""
+--Translation missing
+-- L["no_permission_to_edit_officer_note"] = ""
+--Translation missing
+-- L["rc_version_below_min_notification"] = ""
+--Translation missing
+-- L["RCEPGP_desc"] = ""
+--Translation missing
+-- L["send_epgp_setting_desc"] = ""
+--Translation missing
+-- L["send_epgp_settings"] = ""
+--Translation missing
+-- L["Setting Sync"] = ""
+--Translation missing
+-- L["setting_reset_notification"] = ""
+--Translation missing
+-- L["slash_rc_command_failed"] = ""
+--Translation missing
+-- L["slash_rc_gp_help"] = ""
+--Translation missing
+-- L["slash_rc_gp_help_detailed"] = ""
+--Translation missing
+-- L["slash_rc_undogp_help"] = ""
+--Translation missing
+-- L["slash_rc_undogp_help_detailed"] = ""
 L["slot_weights"] = "Вес слотов"
-L["variable_equipLoc_help"] = "Строка. Не локализованная строка с названием слота экипировки. Рекомендуется использовать переменную \"slotWeights\" вместо нее"
-L["variable_hasAvoid_help"] = "Целочисленное значение. 1 - если на предмете есть избегание, 0 в противном случае."
-L["variable_hasIndes_help"] = "Целочисленное значение. 1 - если предмет не теряет прочности, 0 в противном случае."
-L["variable_hasLeech_help"] = "Целочисленное значение. 1 - если на предмете есть самоисцеление, 0 в противном случае."
-L["variable_hasSpeed_help"] = "Целочисленное значение. 1 - если предмет c дополнительной скоростью передвижения, 0 в противном случае."
-L["variable_ilvl_help"] = "Целочисленное значение. Уровень предмета или базовый ilvl токена."
-L["variable_isToken_help"] = "Целочисленное значение. 1 - если это тировый токен, 0 в противном случае."
-L["variable_itemID_help"] = "Целочисленное значение. Id предмета."
-L["variable_numSocket_help"] = "Целочисленное значение. Количество гнезд в предмете."
-L["variable_rarity_help"] = "Целочисленное значение. Редкость предмета. 3-редкий, 4-эпический, 5-легендарный"
-L["variable_slotWeights_help"] = "Число. Вес предмета в зависимости от слота экипировки."
-L["disable_gp_popup"] = "Всплывающее окно GP автоматически отключается с помощью RCLootCouncil - EPGP."
-L["GP Bid"] = "ставку GP"
-L["Custom GP"] = "пользовательское значение GP"
-L["Enable Bidding"] = "разрешать ставки"
-L["Bidding"] = "ставки"
-
--- Need translation
---L["bidding_desc"] = "Player can send bid price to the loot master by sending a note that starts with integer."
-L["Credit GP to %s"] = "Начислить GP для игрока %s"
 L["Undo GP"] = "Отменить GP"
-
--- Need translation
---L["Award GP (Default: %s)"]
---L["variable_link_help"] 
---L["chat_commands"]
---L["gpOptions"]
---L["gpOptionsButton"]
---L["variable_isNormal_help"]
---L["variable_isHeroic_help"]
---L["variable_isMythic_help"]
---L["variable_isWarforged_help"]
---L["variable_isTitanforged_help"]
