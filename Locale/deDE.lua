68267a749aee2aeea7f87c7a93240415a8f142d4
SafeteeWoW <30888549+SafeteeWoW@users.noreply.github.com>
2017-10-14 12:22:09 -0700

    Eliminate all localization string in the source. Use strings stored on Curseforge for the development.

--- Locale/deDE.lua
+++ Locale/deDE.lua
@@ -4,10 +4,4 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("RCEPGP", "deDE")
 if not L then return end
 
--- Manually export from Curse here if you want to update the addon from Github, but not guarantee to be update-to-date.
---@debug@
-
---@end-debug@
-
-
 --@localization(locale="deDE", format="lua_additive_table", same-key-is-true=true)@
