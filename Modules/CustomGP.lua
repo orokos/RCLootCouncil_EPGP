208d2da7eaf7ca90770493f18d8ccf0500e03586
SafeteeWoW <30888549+SafeteeWoW@users.noreply.github.com>
2017-10-31 01:53:07 -0700

    ItemStatusText cache code cleanup

--- Modules/CustomGP.lua
+++ Modules/CustomGP.lua
@@ -285,14 +285,16 @@ local statusTextItems =
 -- store the item status text in the saved variable\
 -- @param return if success. Should call this function again if failed.
 function RCCustomGP:LocalizeItemStatusText()
-	if not addon.db.global.localizedItemStatus or addon.db.global.localizedItemStatus.locale ~= GetLocale() then
+	if not addon.db.global.localizedItemStatus then
 		addon.db.global.localizedItemStatus = {}
-		addon.db.global.localizedItemStatus.locale = GetLocale()
+	end
+	if addon.db.global.localizedItemStatus.created ~= GetLocale() then
+		addon.db.global.localizedItemStatus.created = false
 	end
 
 	local success = true
 	for key, item in pairs(statusTextItems) do
-		if not addon.db.global.localizedItemStatus[key] then
+		if not addon.db.global.localizedItemStatus[key] or not addon.db.global.localizedItemStatus.created then
 			GetItemInfo(item)
 			addon.db.global.localizedItemStatus[key] = GetTextLeft2(item)
 			if not addon.db.global.localizedItemStatus[key] or addon.db.global.localizedItemStatus[key] == "" then
@@ -302,6 +304,9 @@ function RCCustomGP:LocalizeItemStatusText()
 		end
 	end
 
+	if success then
+		addon.db.global.localizedItemStatus.created = GetLocale()
+	end
 	return success
 end
 
